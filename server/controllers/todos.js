d3983fc23eb1a2239661e2f6714c00622018aa9f
benNjogu <kamandemachafi@gmail.com>
2023-03-27 20:39:33 +0300

    Returning updated post

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -17,7 +17,48 @@ const createTODO = async (req, res) => {
 const getTODO = async (req, res) => {
   try {
     const todos = await pool.query("SELECT * FROM todo");
+
     res.json(todos.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const getTODOById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const todo = await pool.query("SELECT * FROM todo WHERE t_id = $1", [id]);
+
+    res.json(todo.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const updateTODO = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+
+    const updatedToDo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE t_id = $2 RETURNING *",
+      [description, id]
+    );
+
+    res.json(updatedToDo.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const deleteTODO = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    pool.query("DELETE FROM todo WHERE t_id = $1", [id]);
+
+    res.json("deleted!");
   } catch (error) {
     console.log(error.message);
   }
@@ -26,4 +67,7 @@ const getTODO = async (req, res) => {
 module.exports = {
   createTODO,
   getTODO,
+  getTODOById,
+  updateTODO,
+  deleteTODO,
 };
--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,10 +1,19 @@
 const express = require("express");
 
-const { createTODO, getTODO } = require("../controllers/todos.js");
+const {
+  createTODO,
+  getTODO,
+  getTODOById,
+  updateTODO,
+  deleteTODO,
+} = require("../controllers/todos.js");
 
 const router = express.Router();
 
 router.post("/", createTODO);
 router.get("/", getTODO);
+router.get("/:id", getTODOById);
+router.put("/:id", updateTODO);
+router.delete("/:id", deleteTODO);
 
 module.exports = router;
